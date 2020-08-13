d0ff9238d077205f99ce5c5b6ef2e5ee84e2ba9e
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2020-08-13 23:18:28 +0300

    Initial Commit

--- KutuphaneSistemi/Program.cs
+++ KutuphaneSistemi/Program.cs
@@ -16,7 +16,7 @@ namespace KutuphaneSistemi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ());
+            Application.Run(new Form1());
         }
     }
 }
