d234e03a87b2f0d180efb21a0dda40a37d3c203a
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-12-15 23:32:54 +0300

    Update

--- KutuphaneSistemi/Properties/Resources.Designer.cs
+++ KutuphaneSistemi/Properties/Resources.Designer.cs
@@ -3,7 +3,7 @@ namespace KutuphaneSistemi.Properties
 
 
     /// <summary>
-    ///   A strongly-typed resource class, for looking up localized strings, etc.
+
     /// </summary>
  
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
