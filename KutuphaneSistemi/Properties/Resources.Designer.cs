60730dc91592e89e927da6fdc03ed7b338b945d9
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-12-16 23:18:02 +0300

    Update

--- KutuphaneSistemi/Properties/Resources.Designer.cs
+++ KutuphaneSistemi/Properties/Resources.Designer.cs
@@ -39,8 +39,7 @@ namespace KutuphaneSistemi.Properties
         }
 
         /// <summary>
-        ///   Overrides the current thread's CurrentUICulture property for all
-        ///   resource lookups using this strongly typed resource class.
+     
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Globalization.CultureInfo Culture
